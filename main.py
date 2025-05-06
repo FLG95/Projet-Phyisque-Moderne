66c4fd35509e111e98365113be7d4abfd59d6868
paulmrll <paul.morille11@gmail.com>
2025-05-06 10:29:24 +0200

    changes

---
+++ main.py
@@ -0,0 +1,19 @@
+print("hello World")
+print("hello World")
+print("hello World")
+print("hello World")
+from math import *
+
+print("hello World")
+print("hello World")
+print("hello World")
+print("hello World")
+print("hello World")
+for i in range(10):
+    print(i)
+def add(a, b):
+    return a + b
+print(add(1, 2))
+
+
+#il faut prendre E > V0
